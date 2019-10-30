4f4b649caa64cd1a87feec0dd12309e9f0d10412
Pablo Adoue <pabloadoue@Pablos-MacBook-Pro.local>
2019-10-30 13:17:44 -0600

    Added a patch for consistency with the hr.employee.public field

--- hyper-hr/models/models.py
+++ hyper-hr/models/models.py
@@ -41,6 +41,26 @@ class HrEmployee(models.Model):
         _logger.debug("Updated")
         return super().write(values)
     
+class HrEmployeePublic(models.Model):
+    _inherit = "hr.employee.public"
+
+    #File Fields
+    file_birth_certificate=fields.Binary("Acta de Nacimiento")
+    file_address_certificate=fields.Binary("Comprobante de Domicilio")
+    file_curp=fields.Binary("Certificado CURP")
+    file_social_security=fields.Binary("Inscripción NSS")
+    file_job_description=fields.Binary("Descripción de Puesto")
+    file_bank_statement=fields.Binary("Estado de Cuenta")
+    file_school_certificate=fields.Binary("Comprobante de Estudios")
+    file_id=fields.Binary("Identificación")
+    file_tax_id=fields.Binary("Cédula Fiscal")
+    file_kardex=fields.Binary("Kardex")
+
+    #other fields
+    no_rfc=fields.Char("RFC")
+    curp=fields.Char("CURP")
+    nss=fields.Char("NSS")
+    status=fields.Selection(EMPLOYEE_STATUS_SELECTION,default='new')    
 #    @api.multi
 #    def write(self, vals):
 #        return super(ResPartner, self).write(values)
