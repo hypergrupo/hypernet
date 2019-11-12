51b2370358b1cb12913d8f5e7de3ec184c50bba7
Pablo Adoue <pabloadoue@Pablos-MacBook-Pro.local>
2019-11-12 14:05:48 -0600

    Added Mantainance module

---
+++ hyper-mantainance/models/mantainance_request.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from botocore.exceptions import ClientError
+import boto3
+from odoo import models, fields, api
+
+import logging
+_logger = logging.getLogger(__name__)
+
+
+EMPLOYEE_STATUS_SELECTION = [
+    ('new', 'Nuevo'), ('hired', 'Contratado'), ('fired', 'Baja')]
+
+
+class HrEmployeePublic(models.Model):
+    _inherit = "hr.employee"
