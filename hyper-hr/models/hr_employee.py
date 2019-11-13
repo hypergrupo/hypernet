575e4707ae64acc8ad64a495fe6c055822d63606
Pablo Adoue <pabloadoue@Pablos-MacBook-Pro.local>
2019-11-13 11:50:46 -0600

    Remmoved doble quotes and replaced them with single quotes

--- hyper-hr/models/hr_employee.py
+++ hyper-hr/models/hr_employee.py
@@ -13,32 +13,32 @@ EMPLOYEE_STATUS_SELECTION = [
 
 
 class HrEmployee(models.Model):
-    _inherit = "hr.employee"
+    _inherit = 'hr.employee'
 
     # File Fields
-    file_birth_certificate = fields.Binary("Acta de Nacimiento")
-    file_address_certificate = fields.Binary("Comprobante de Domicilio")
-    file_curp = fields.Binary("Certificado CURP")
-    file_social_security = fields.Binary("Inscripción NSS")
-    file_job_description = fields.Binary("Descripción de Puesto")
-    file_bank_statement = fields.Binary("Estado de Cuenta")
-    file_school_certificate = fields.Binary("Comprobante de Estudios")
-    file_id = fields.Binary("Identificación")
-    file_tax_id = fields.Binary("Cédula Fiscal")
-    file_kardex = fields.Binary("Kardex")
+    file_birth_certificate = fields.Binary('Acta de Nacimiento')
+    file_address_certificate = fields.Binary('Comprobante de Domicilio')
+    file_curp = fields.Binary('Certificado CURP')
+    file_social_security = fields.Binary('Inscripción NSS')
+    file_job_description = fields.Binary('Descripción de Puesto')
+    file_bank_statement = fields.Binary('Estado de Cuenta')
+    file_school_certificate = fields.Binary('Comprobante de Estudios')
+    file_id = fields.Binary('Identificación')
+    file_tax_id = fields.Binary('Cédula Fiscal')
+    file_kardex = fields.Binary('Kardex')
 
     # other fields
     no_rfc = fields.Char(string='RFC', copy=False)
-    curp = fields.Char("CURP")
-    nss = fields.Char("NSS")
+    curp = fields.Char('CURP')
+    nss = fields.Char('NSS')
     status = fields.Selection(EMPLOYEE_STATUS_SELECTION, default='new')
 
-    #change
+    # change
     @api.model
     def create(self, values):
-        _logger.debug("Created")
+        _logger.debug('Created')
         return super().create(values)
 
     def write(self, values):
-        _logger.debug("Updated")
+        _logger.debug('Updated')
         return super().write(values)
