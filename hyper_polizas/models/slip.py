de2c88793b20b114bff596feec34614e0f308937
Pablo Adoue <pabloadoue@Pablos-MacBook-Pro.local>
2019-12-04 14:00:14 -0600

    Added basic module functionality

--- hyper_polizas/models/slip.py
+++ hyper_polizas/models/slip.py
@@ -1,19 +1,35 @@
 # -*- coding: utf-8 -*-
 
- from odoo import models, fields, api
+from odoo import models, fields, api
 
+PREMIUM_MODALITY = [
+    ('12', 'Monthly'), ('4', 'Trimestral'), ('2', 'Semestral'), ('1', 'Anual')]
+PAYMENT_METHOD = [
+    ('credit', 'Tarjeta de Crédito'), ('debit', 'Tarjeta de Débito'), ('pos', 'Punto de Venta')]
 
 class slip(models.Model):
-     _name = 'slip'
-     _description = 'Module for creating'
+    _name = 'slip'
+    _description = 'Module for creating'
 
-     cancellation_date=fields.Date(string="Fecha de Cancelación")
-     car_licence_plate=fields.Char(string="Placas")
-     car_make=fields.Char(string="Marca")
-     car_model=fields.Char(string="Modelo")
-     car_vin=fields.Char(string="Número de Serie")
-     car_year=fields.Integer(string="Año")
-     expiration_date=fields.Date(string="Fin de Vigencia")
+    car_licence_plate=fields.Char(string="Placas")
+    car_make=fields.Char(string="Marca")
+    car_model=fields.Char(string="Modelo")
+    car_vin=fields.Char(string="Número de Serie")
+    car_year=fields.Integer(string="Año")
+     
+    premium=fields.Float(string="Prima")
+    initial_premium=fields.Float(string="Prima Inicial")
+    following_premium=fields.Float(string="Fin de Vigencia")
+     
+    issue_date=fields.Date(string="Fecha de Emisión")
+    in_force_date=fields.Date(string="Inicio de Vigencia")
+    cancellation_date=fields.Date(string="Fecha de Cancelación")
+    expiration_date=fields.Date(string="Fin de Vigencia")
+    
+    premium_modality = fields.Selection(PREMIUM_MODALITY, copy=False, index=0, domain=[(1, '=', 1)])
+    payment_method= fields.Selection(PAYMENT_METHOD, copy=False, index=0, domain=[(1, '=', 1)])
+
+    policy_number=fields.Char(string="Número de Póliza")
 
 
 
