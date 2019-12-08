f4ba4c8ef2c8db144222bc722680930129d49c15
Pablo Adoue <pabloadoue@Pablos-MacBook-Pro.local>
2019-12-08 13:14:53 -0600

    Changed default value for Job Role

--- hyper_polizas/models/partner.py
+++ hyper_polizas/models/partner.py
@@ -10,7 +10,7 @@ class Partner(models.Model):
 
     is_client = fields.Boolean(string='Es Cliente', copy=True, index=False, domain=[(1, '=', 1)])
     is_insurer = fields.Boolean(string='Es Aseguradora', copy=True, index=False, domain=[(1, '=', 1)])
-    job_role = fields.Selection(EMPLOYEE_JOB_ROL, copy=False, index=True, domain=[(1, '=', 1)], default='sales')
+    job_role = fields.Selection(EMPLOYEE_JOB_ROL, copy=False, index=True, domain=[(1, '=', 1)])
     
 #     name = fields.Char()
 #     value = fields.Integer()
